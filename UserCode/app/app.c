cdba4eb54b632173c5b9da0e1ecbd512895183fa
syhanjin <syhanjin666@gmail.com>
2025-09-03 22:43:13 +0800

    init commit with basic project structure

---
+++ UserCode/app/app.c
@@ -0,0 +1,10 @@
+/**
+ * @file    app.c
+ * @author  syhanjin
+ * @date    2025-09-03
+ * @brief   Brief description of the file
+ *
+ * Detailed description (optional).
+ *
+ */
+#include "app.h"
---
+++ UserCode/app/app.h
@@ -0,0 +1,13 @@
+/**
+ * @file    app.h
+ * @author  syhanjin
+ * @date    2025-09-03
+ * @brief   Brief description of the file
+ *
+ * Detailed description (optional).
+ *
+ */
+#ifndef APP_H
+#define APP_H
+
+#endif //APP_H
