e159e91d10c14421bc0ce68ab8d861d239512e68
syhanjin <syhanjin666@gmail.com>
2025-09-06 17:24:22 +0800

    test: update test example to fit new interface

--- UserCode/app/app.c
+++ UserCode/app/app.c
@@ -12,18 +12,19 @@
 #include "bsp/can_driver.h"
 #include "can.h"
 #include "cmsis_os2.h"
-#include "controllers/dji_driver.h"
 #include "drivers/DJI.h"
+#include "interfaces/motor_if.h"
 #include "tim.h"
 
-DJI_PosCtrl_t pos_dji;
-DJI_VelCtrl_t vel_dji;
+DJI_t dji[2];
+Motor_PosCtrl_t pos_dji;
+Motor_VelCtrl_t vel_dji;
 
 void TIM6_Callback(TIM_HandleTypeDef* htim)
 {
     /* 进行 PID 计算 */
-    DJI_PosCtrlCalculate(&pos_dji);
-    DJI_VelCtrlCalculate(&vel_dji);
+    Motor_PosCtrlCalculate(&pos_dji);
+    Motor_VelCtrlCalculate(&vel_dji);
 
     /* 发送控制信号 */
     DJI_SendSetIqCommand(&hcan1, IQ_CMD_GROUP_1_4);
@@ -46,46 +47,54 @@ void Init(void* argument)
     HAL_CAN_RegisterCallback(&hcan1, HAL_CAN_RX_FIFO0_MSG_PENDING_CB_ID, DJI_CAN_Fifo0ReceiveCallback);
     // HAL_CAN_RegisterCallback(&hcan1, HAL_CAN_RX_FIFO1_MSG_PENDING_CB_ID, DJI_CAN_Fifo1ReceiveCallback);
 
+    // 启动 CAN 接收
     // CAN_Start(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING | CAN_IT_RX_FIFO1_MSG_PENDING);
     CAN_Start(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 
-    DJI_PosCtrl_Init(&pos_dji, //
-                     (DJI_PosCtrlConfig_t){
-                         .dji = (DJI_Config_t){
-                             .auto_zero  = false, // 是否在启动时自动清零角度
-                             .hcan       = &hcan1,
-                             .motor_type = M3508_C620,
-                             .id1        = 1, // 电调 ID (1~8)
-                         },
-                         .velocity_pid = (MotorPID_Config_t){
-                             .Kp             = 12.0f,  //
-                             .Ki             = 0.20f,  //
-                             .Kd             = 5.00f,  //
-                             .abs_output_max = 8000.0f // DJI_M3508_C620_IQ_MAX //< 限幅为电流控制最大值
-                         },
-                         .position_pid = (MotorPID_Config_t){
-                             .Kp             = 80.0f,  //
-                             .Ki             = 1.00f,  //
-                             .Kd             = 0.00f,  //
-                             .abs_output_max = 2000.0f // 限速
-                         },
-                         .pos_vel_freq_ratio = 1});
+    /* 初始化电机 */
+    DJI_Init(&dji[0], (DJI_Config_t){
+                          .auto_zero  = false, // 是否在启动时自动清零角度
+                          .hcan       = &hcan1,
+                          .motor_type = M3508_C620,
+                          .id1        = 1, // 电调 ID (1~8)
+                      });
+    DJI_Init(&dji[1], (DJI_Config_t){
+                          .auto_zero  = false, // 是否在启动时自动清零角度
+                          .hcan       = &hcan1,
+                          .motor_type = M3508_C620,
+                          .id1        = 2, // 电调 ID (1~8)
+                      });
 
-    DJI_VelCtrl_Init(&vel_dji, //
-                     (DJI_VelCtrlConfig_t){
-                         .dji = (DJI_Config_t){
-                             .auto_zero  = false, // 是否在启动时自动清零角度
-                             .hcan       = &hcan1,
-                             .motor_type = M3508_C620,
-                             .id1        = 2, // 电调 ID (1~8)
-                         },
-                         .pid = (MotorPID_Config_t){
-                             .Kp             = 4.7f,                 //
-                             .Ki             = 0.15f,                //
-                             .Kd             = 0.15f,                //
-                             .abs_output_max = DJI_M3508_C620_IQ_MAX //< 限幅为电流控制最大值
-                         },
-                     });
+    /* 初始化电机控制 */
+    Motor_PosCtrl_Init(&pos_dji, //
+                       (Motor_PosCtrlConfig_t){
+                           .motor_type   = MOTOR_TYPE_DJI, //< 电机类型
+                           .motor        = &dji[0],        //< 控制的电机
+                           .velocity_pid = (MotorPID_Config_t){
+                               .Kp             = 12.0f,  //
+                               .Ki             = 0.20f,  //
+                               .Kd             = 5.00f,  //
+                               .abs_output_max = 8000.0f // DJI_M3508_C620_IQ_MAX //< 限幅为电流控制最大值
+                           },
+                           .position_pid = (MotorPID_Config_t){
+                               .Kp             = 80.0f,  //
+                               .Ki             = 1.00f,  //
+                               .Kd             = 0.00f,  //
+                               .abs_output_max = 2000.0f // 限速
+                           },
+                           .pos_vel_freq_ratio = 1});
+
+    Motor_VelCtrl_Init(&vel_dji, //
+                       (Motor_VelCtrlConfig_t){
+                           .motor_type = MOTOR_TYPE_DJI, //< 电机类型
+                           .motor      = &dji[1],        //< 控制的电机
+                           .pid        = (MotorPID_Config_t){
+                                      .Kp             = 4.7f,                 //
+                                      .Ki             = 0.15f,                //
+                                      .Kd             = 0.15f,                //
+                                      .abs_output_max = DJI_M3508_C620_IQ_MAX //< 限幅为电流控制最大值
+                           },
+                       });
 
     // 注册定时器回调
     HAL_TIM_RegisterCallback(&htim6, HAL_TIM_PERIOD_ELAPSED_CB_ID, TIM6_Callback);
