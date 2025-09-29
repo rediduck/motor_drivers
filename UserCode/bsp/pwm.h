d8b8105ad63a8c9d4e43ba7f9593cecfbc60fc93
syhanjin <syhanjin666@gmail.com>
2025-09-29 13:33:18 +0800

    style(bsp): using __HAL_TIM_GET_AUTORELOAD

--- UserCode/bsp/pwm.h
+++ UserCode/bsp/pwm.h
@@ -21,7 +21,7 @@ static inline void PWM_Stop(PWM_t* hpwm) { HAL_TIM_PWM_Stop(hpwm->htim, hpwm->ch
 
 static inline void PWM_SetCompare(PWM_t* hpwm, const uint32_t compare)
 {
-    if (compare <= hpwm->htim->Instance->ARR)
+    if (compare <= __HAL_TIM_GET_AUTORELOAD(hpwm->htim))
         __HAL_TIM_SET_COMPARE(hpwm->htim, hpwm->channel, compare);
 }
 
@@ -30,9 +30,9 @@ static inline void PWM_SetDutyCircle(PWM_t* hpwm, const float duty_circle)
     if (duty_circle < 0.0f)
         PWM_SetCompare(hpwm, 0);
     else if (duty_circle > 1.0f)
-        PWM_SetCompare(hpwm, hpwm->htim->Instance->ARR);
+        PWM_SetCompare(hpwm, __HAL_TIM_GET_AUTORELOAD(hpwm->htim));
     else
-        PWM_SetCompare(hpwm, hpwm->htim->Instance->ARR * duty_circle + 0.5);
+        PWM_SetCompare(hpwm, __HAL_TIM_GET_AUTORELOAD(hpwm->htim) * duty_circle + 0.5);
 }
 
 #endif // PWM_H
