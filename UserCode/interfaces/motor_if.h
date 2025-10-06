82c83cd61115737898fcb0006e03c8c4f505283b
syhanjin <syhanjin666@gmail.com>
2025-10-06 16:05:29 +0800

    feat: add GPLv3 license headers to all files

--- UserCode/interfaces/motor_if.h
+++ UserCode/interfaces/motor_if.h
@@ -8,6 +8,22 @@
  *   - drivers/DJI.h: 大疆电机
  *   - drivers/tb6612.h: 由 TB6612 芯片驱动的直流电机
  *   - drivers/vesc.h: VESC 电调驱动的电机
+ *
+ * --------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project repository: https://github.com/HITSZ-WTR2026/motor_drivers
  */
 #ifndef MOTOR_IF_H
 #define MOTOR_IF_H
--- UserCode/libs/pid_motor.h
+++ UserCode/libs/pid_motor.h
@@ -6,6 +6,23 @@
  *
  * 本库只包含基本的 比例 - 积分 - 微分 控制以及抗饱和算法，不包括其他复杂算法，用于电机控制
  *
+ * Detailed description (optional).
+ *
+ * --------------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project repository: https://github.com/HITSZ-WTR2026/motor_drivers
  */
 #ifndef PID_MOTOR_H
 #define PID_MOTOR_H
