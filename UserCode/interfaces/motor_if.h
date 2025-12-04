82bcf3675ebbfd7a98e748d6cb167ba68c1c7431
syhanjin <syhanjin666@gmail.com>
2025-12-04 14:31:21 +0800

    style: reformat motor_if.h

--- UserCode/interfaces/motor_if.h
+++ UserCode/interfaces/motor_if.h
@@ -81,34 +81,34 @@ extern "C"
 {
 #endif
 
-    typedef enum
-    {
+typedef enum
+{
 #ifdef USE_DJI
-        MOTOR_TYPE_DJI, //< 大疆电机，依赖 drivers/DJI.h
+    MOTOR_TYPE_DJI, //< 大疆电机，依赖 drivers/DJI.h
 #endif
 #ifdef USE_TB6612
-        MOTOR_TYPE_TB6612,
+    MOTOR_TYPE_TB6612,
 #endif
 #ifdef USE_VESC
-        MOTOR_TYPE_VESC,
+    MOTOR_TYPE_VESC,
 #endif
 #ifdef USE_DM
-        MOTOR_TYPE_DM,
+    MOTOR_TYPE_DM,
 #endif
-    } MotorType_t;
-    /**
-     * 电机控制模式
-     */
-    typedef enum
-    {
-        MOTOR_CTRL_EXTERNAL_PID, ///< 完全外部 PID 控制
+} MotorType_t;
+/**
+ * 电机控制模式
+ */
+typedef enum
+{
+    MOTOR_CTRL_EXTERNAL_PID, ///< 完全外部 PID 控制
 #ifdef MOTOR_IF_INTERNAL_VEL
-        MOTOR_CTRL_INTERNAL_VEL, ///< 内部速度环控制 + 外部位置环控制
+    MOTOR_CTRL_INTERNAL_VEL, ///< 内部速度环控制 + 外部位置环控制
 #endif
 #ifdef MOTOR_IF_INTERNAL_VEL_POS
-        MOTOR_CTRL_INTERNAL_VEL_POS, ///< 内部位置环和速度环控制
+    MOTOR_CTRL_INTERNAL_VEL_POS, ///< 内部位置环和速度环控制
 #endif
-    } MotorCtrlMode_t;
+} MotorCtrlMode_t;
 
 // 电机控制模式的默认值
 #if defined(USE_DJI) && !defined(MOTOR_DEFAULT_MODE_DJI)
@@ -129,78 +129,78 @@ extern "C"
 
 #endif
 
-    /**
-     * 位置环控制对象
-     */
-    typedef struct
+/**
+ * 位置环控制对象
+ */
+typedef struct
+{
+    bool            enable;             ///< 是否启用控制
+    MotorType_t     motor_type;         ///< 受控电机类型
+    MotorCtrlMode_t ctrl_mode;          ///< 控制模式
+    void*           motor;              ///< 受控电机
+    MotorPID_t      velocity_pid;       ///< 内环，速度环
+    MotorPID_t      position_pid;       ///< 外环，位置环
+    uint32_t        pos_vel_freq_ratio; ///< 内外环频率比
+    uint32_t        count;              ///< 计数
+    float           position;           ///< 当前控制的位置
+
+    struct
     {
-        bool            enable;             ///< 是否启用控制
-        MotorType_t     motor_type;         ///< 受控电机类型
-        MotorCtrlMode_t ctrl_mode;          ///< 控制模式
-        void*           motor;              ///< 受控电机
-        MotorPID_t      velocity_pid;       ///< 内环，速度环
-        MotorPID_t      position_pid;       ///< 外环，位置环
-        uint32_t        pos_vel_freq_ratio; ///< 内外环频率比
-        uint32_t        count;              ///< 计数
-        float           position;           ///< 当前控制的位置
+        float    error_threshold; ///< 允许的误差范围
+        uint32_t count_max;       ///< 保持的计数范围
+        uint32_t counter;         ///< 就位计数
+    } settle;                     ///< 就位判断
 
-        struct
-        {
-            float    error_threshold; ///< 允许的误差范围
-            uint32_t count_max;       ///< 保持的计数范围
-            uint32_t counter;         ///< 就位计数
-        } settle;                     ///< 就位判断
+} Motor_PosCtrl_t;
 
-    } Motor_PosCtrl_t;
-
-    /**
-     * 位置环控制配置
-     */
-    typedef struct
-    {
-        MotorType_t motor_type; ///< 受控电机类型
+/**
+ * 位置环控制配置
+ */
+typedef struct
+{
+    MotorType_t motor_type; ///< 受控电机类型
 #ifdef USE_CUSTOM_CTRL_MODE
-        MotorCtrlMode_t ctrl_mode; ///< 控制模式
+    MotorCtrlMode_t ctrl_mode; ///< 控制模式
 #endif
-        void*             motor; ///< 受控电机
-        MotorPID_Config_t velocity_pid;
-        MotorPID_Config_t position_pid;
-        uint32_t          pos_vel_freq_ratio; ///< 内外环频率比
+    void*             motor; ///< 受控电机
+    MotorPID_Config_t velocity_pid;
+    MotorPID_Config_t position_pid;
+    uint32_t          pos_vel_freq_ratio; ///< 内外环频率比
 
-        float    error_threshold;  ///< 允许的误差范围
-        uint32_t settle_count_max; ///< 在误差内多少周期认为就位
-    } Motor_PosCtrlConfig_t;
+    float    error_threshold;  ///< 允许的误差范围
+    uint32_t settle_count_max; ///< 在误差内多少周期认为就位
+} Motor_PosCtrlConfig_t;
 
-    /**
-     * 速度环控制对象
-     */
-    typedef struct
-    {
-        bool            enable;     //< 是否启用控制
-        MotorType_t     motor_type; //< 受控电机类型
-        MotorCtrlMode_t ctrl_mode;  ///< 控制模式
-        void*           motor;      //< 受控电机
-        MotorPID_t      pid;        //< 速度环
-        float           velocity;   //< 当前控制的速度
-    } Motor_VelCtrl_t;
+/**
+ * 速度环控制对象
+ */
+typedef struct
+{
+    bool            enable;     //< 是否启用控制
+    MotorType_t     motor_type; //< 受控电机类型
+    MotorCtrlMode_t ctrl_mode;  ///< 控制模式
+    void*           motor;      //< 受控电机
+    MotorPID_t      pid;        //< 速度环
+    float           velocity;   //< 当前控制的速度
+} Motor_VelCtrl_t;
 
-    /**
-     * 速度环控制配置
-     */
-    typedef struct
-    {
-        MotorType_t motor_type; //< 受控电机类型
+/**
+ * 速度环控制配置
+ */
+typedef struct
+{
+    MotorType_t motor_type; //< 受控电机类型
 #ifdef USE_CUSTOM_CTRL_MODE
-        MotorCtrlMode_t ctrl_mode; ///< 控制模式
+    MotorCtrlMode_t ctrl_mode; ///< 控制模式
 #endif
-        void*             motor; //< 受控电机
-        MotorPID_Config_t pid;
-    } Motor_VelCtrlConfig_t;
+    void*             motor; //< 受控电机
+    MotorPID_Config_t pid;
+} Motor_VelCtrlConfig_t;
 
-    void Motor_PosCtrl_Init(Motor_PosCtrl_t* hctrl, const Motor_PosCtrlConfig_t* config);
-    void Motor_VelCtrl_Init(Motor_VelCtrl_t* hctrl, const Motor_VelCtrlConfig_t* config);
-    void Motor_PosCtrlUpdate(Motor_PosCtrl_t* hctrl);
-    void Motor_VelCtrlUpdate(Motor_VelCtrl_t* hctrl);
+void Motor_PosCtrl_Init(Motor_PosCtrl_t* hctrl, const Motor_PosCtrlConfig_t* config);
+void Motor_VelCtrl_Init(Motor_VelCtrl_t* hctrl, const Motor_VelCtrlConfig_t* config);
+void Motor_PosCtrlUpdate(Motor_PosCtrl_t* hctrl);
+void Motor_VelCtrlUpdate(Motor_VelCtrl_t* hctrl);
 
 /**
  * 启用电机控制
@@ -214,159 +214,158 @@ extern "C"
  */
 #define __MOTOR_CTRL_DISABLE(__CTRL_HANDLE__) ((__CTRL_HANDLE__)->enable = false)
 
-    /**
-     * 设置阈值
-     * @param hctrl 受控对象
-     * @param threshold 阈值
-     */
-    static inline void Motor_PosCtrl_SetErrorThreshold(Motor_PosCtrl_t* hctrl,
-                                                       const float      threshold)
-    {
-        if (threshold > 0)
-            hctrl->settle.error_threshold = threshold;
-    }
+/**
+ * 设置阈值
+ * @param hctrl 受控对象
+ * @param threshold 阈值
+ */
+static inline void Motor_PosCtrl_SetErrorThreshold(Motor_PosCtrl_t* hctrl, const float threshold)
+{
+    if (threshold > 0)
+        hctrl->settle.error_threshold = threshold;
+}
 
-    /**
-     * 判断电机位置环控制是否就位
-     * @param hctrl 受控对象
-     * @return 是否就位
-     */
-    static inline bool Motor_PosCtrl_IsSettle(Motor_PosCtrl_t* hctrl)
-    {
-        return hctrl->settle.counter >= hctrl->settle.count_max;
-    }
+/**
+ * 判断电机位置环控制是否就位
+ * @param hctrl 受控对象
+ * @return 是否就位
+ */
+static inline bool Motor_PosCtrl_IsSettle(Motor_PosCtrl_t* hctrl)
+{
+    return hctrl->settle.counter >= hctrl->settle.count_max;
+}
 
-    /**
-     * 设置位置环目标值
-     * @param hctrl 受控对象
-     * @param ref 目标值 (unit: deg)
-     */
-    static inline void Motor_PosCtrl_SetRef(Motor_PosCtrl_t* hctrl, const float ref)
-    {
-        hctrl->position = ref;
+/**
+ * 设置位置环目标值
+ * @param hctrl 受控对象
+ * @param ref 目标值 (unit: deg)
+ */
+static inline void Motor_PosCtrl_SetRef(Motor_PosCtrl_t* hctrl, const float ref)
+{
+    hctrl->position = ref;
 #ifdef MOTOR_IF_INTERNAL_VEL_POS
-        if (hctrl->ctrl_mode == MOTOR_CTRL_INTERNAL_VEL_POS)
-        { // 在内部位置环控制模式下，需要在设置时立刻同步一次指令
-            Motor_PosCtrlUpdate(hctrl);
-        }
-#endif
+    if (hctrl->ctrl_mode == MOTOR_CTRL_INTERNAL_VEL_POS)
+    { // 在内部位置环控制模式下，需要在设置时立刻同步一次指令
+        Motor_PosCtrlUpdate(hctrl);
     }
+#endif
+}
 
-    /**
-     * 设置速度环目标值
-     * @param hctrl 受控对象
-     * @param ref 目标值 (unit: rpm)
-     */
-    static inline void Motor_VelCtrl_SetRef(Motor_VelCtrl_t* hctrl, const float ref)
-    {
-        hctrl->velocity = ref;
+/**
+ * 设置速度环目标值
+ * @param hctrl 受控对象
+ * @param ref 目标值 (unit: rpm)
+ */
+static inline void Motor_VelCtrl_SetRef(Motor_VelCtrl_t* hctrl, const float ref)
+{
+    hctrl->velocity = ref;
 #if defined(MOTOR_IF_INTERNAL_VEL_POS) || defined(MOTOR_IF_INTERNAL_VEL)
-        switch (hctrl->ctrl_mode)
-        {
+    switch (hctrl->ctrl_mode)
+    {
 #    ifdef MOTOR_IF_INTERNAL_VEL_POS
-        case MOTOR_CTRL_INTERNAL_VEL_POS:
+    case MOTOR_CTRL_INTERNAL_VEL_POS:
 #    endif
 #    ifdef MOTOR_IF_INTERNAL_VEL
-        case MOTOR_CTRL_INTERNAL_VEL:
+    case MOTOR_CTRL_INTERNAL_VEL:
 #    endif
-            // 在内部位置环控制模式以及内部速度环控制模式下，需要在设置时立刻同步一次指令
-            Motor_VelCtrlUpdate(hctrl);
-        default:;
-        }
-#endif
+        // 在内部位置环控制模式以及内部速度环控制模式下，需要在设置时立刻同步一次指令
+        Motor_VelCtrlUpdate(hctrl);
+    default:;
     }
+#endif
+}
 
-    /* 电机反馈量 */
+/* 电机反馈量 */
 
-    /**
-     * 获取电机轴输出角度
-     * @param motor_type 电机类型
-     * @param hmotor 电机数据
-     * @return 电机轴输出角度
-     */
-    static inline float Motor_GetAngle(const MotorType_t motor_type, void* hmotor)
+/**
+ * 获取电机轴输出角度
+ * @param motor_type 电机类型
+ * @param hmotor 电机数据
+ * @return 电机轴输出角度
+ */
+static inline float Motor_GetAngle(const MotorType_t motor_type, void* hmotor)
+{
+    switch (motor_type)
     {
-        switch (motor_type)
-        {
 #ifdef USE_DJI
-        case MOTOR_TYPE_DJI:
-            return __DJI_GET_ANGLE(hmotor);
+    case MOTOR_TYPE_DJI:
+        return __DJI_GET_ANGLE(hmotor);
 #endif
 #ifdef USE_TB6612
-        case MOTOR_TYPE_TB6612:
-            return __TB6612_GET_ANGLE(hmotor);
+    case MOTOR_TYPE_TB6612:
+        return __TB6612_GET_ANGLE(hmotor);
 #endif
 #ifdef USE_VESC
-        case MOTOR_TYPE_VESC:
-            return __VESC_GET_ANGLE(hmotor);
+    case MOTOR_TYPE_VESC:
+        return __VESC_GET_ANGLE(hmotor);
 #endif
 #ifdef USE_DM
-        case MOTOR_TYPE_DM:
-            return __DM_GET_ANGLE(hmotor);
+    case MOTOR_TYPE_DM:
+        return __DM_GET_ANGLE(hmotor);
 #endif
-        default:
-            return 0.0f;
-        }
+    default:
+        return 0.0f;
     }
+}
 
-    static inline void Motor_ResetAngle(const MotorType_t motor_type, void* hmotor)
+static inline void Motor_ResetAngle(const MotorType_t motor_type, void* hmotor)
+{
+    switch (motor_type)
     {
-        switch (motor_type)
-        {
 #ifdef USE_DJI
-        case MOTOR_TYPE_DJI:
-            DJI_ResetAngle(hmotor);
-            break;
+    case MOTOR_TYPE_DJI:
+        DJI_ResetAngle(hmotor);
+        break;
 #endif
 #ifdef USE_TB6612
-        case MOTOR_TYPE_TB6612:
-            __TB6612_RESET_ANGLE(hmotor);
-            break;
+    case MOTOR_TYPE_TB6612:
+        __TB6612_RESET_ANGLE(hmotor);
+        break;
 #endif
 #ifdef USE_VESC
-        case MOTOR_TYPE_VESC:
-            VESC_ResetAngle(hmotor);
-            break;
+    case MOTOR_TYPE_VESC:
+        VESC_ResetAngle(hmotor);
+        break;
 #endif
 #ifdef USE_DM
-        case MOTOR_TYPE_DM:
+    case MOTOR_TYPE_DM:
 
-            break;
+        break;
 #endif
-        default:;
-        }
+    default:;
     }
+}
 
-    /**
-     * 获取电机转速
-     * @param motor_type 电机类型
-     * @param hmotor 电机数据
-     * @return 电机输出转速
-     */
-    static inline float Motor_GetVelocity(const MotorType_t motor_type, void* hmotor)
+/**
+ * 获取电机转速
+ * @param motor_type 电机类型
+ * @param hmotor 电机数据
+ * @return 电机输出转速
+ */
+static inline float Motor_GetVelocity(const MotorType_t motor_type, void* hmotor)
+{
+    switch (motor_type)
     {
-        switch (motor_type)
-        {
 #ifdef USE_DJI
-        case MOTOR_TYPE_DJI:
-            return __DJI_GET_VELOCITY(hmotor);
+    case MOTOR_TYPE_DJI:
+        return __DJI_GET_VELOCITY(hmotor);
 #endif
 #ifdef USE_TB6612
-        case MOTOR_TYPE_TB6612:
-            return __TB6612_GET_VELOCITY(hmotor);
+    case MOTOR_TYPE_TB6612:
+        return __TB6612_GET_VELOCITY(hmotor);
 #endif
 #ifdef USE_VESC
-        case MOTOR_TYPE_VESC:
-            return __VESC_GET_VELOCITY(hmotor);
+    case MOTOR_TYPE_VESC:
+        return __VESC_GET_VELOCITY(hmotor);
 #endif
 #ifdef USE_DM
-        case MOTOR_TYPE_DM:
-            return __DM_GET_VELOCITY(hmotor);
+    case MOTOR_TYPE_DM:
+        return __DM_GET_VELOCITY(hmotor);
 #endif
-        default:
-            return 0.0f;
-        }
+    default:
+        return 0.0f;
     }
+}
 #ifdef __cplusplus
 }
 #endif
